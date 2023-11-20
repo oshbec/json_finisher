2a4d03152cfe7f0f7f3b9702ea3239061446b6b8
Josh Bechard <josh@normalthought.com>
2023-11-20 12:01:33 -0600

    Starting stack builder implementation

--- lib/json_finisher/stack_builder.ex
+++ lib/json_finisher/stack_builder.ex
@@ -1,9 +1,25 @@
 defmodule JsonFinisher.StackBuilder do
   @moduledoc """
-  Builds a stack representing the structure of a partially complete JSON string.
+  Builds a stack representing the structure of a partial JSON string.
   """
 
-  def build_stack(_json_fragment) do
-    []
+  # Handles the case for an empty string
+  def build_stack(json_fragment) when json_fragment == "" do
+    {:ok, []}
   end
+
+  # Handles non-empty strings
+  def build_stack(json_fragment) do
+    json_fragment
+    |> String.graphemes()
+    |> Enum.reduce([], &process_char/2)
+    |> case do
+      {:error, _} = error -> error
+      stack -> {:ok, stack}
+    end
+  end
+
+  # Pattern matching for specific characters
+  defp process_char("{", stack), do: [:object | stack]
+  defp process_char(_, stack), do: stack
 end
--- test/json_finisher/stack_builder_test.exs
+++ test/json_finisher/stack_builder_test.exs
@@ -1,5 +1,27 @@
 defmodule JsonFinisher.StackBuilderTest do
   use ExUnit.Case, async: true
+  alias JsonFinisher.StackBuilder
 
-  # Tests for JsonFinisher.StackBuilder module
+  test "stack for empty string should be empty" do
+    assert StackBuilder.build_stack("") == {:ok, []}
+  end
+
+  test "stack for '{' should be [:object]" do
+    assert StackBuilder.build_stack("{") == {:ok, [:object]}
+  end
+
+  @tag :skip
+  test "stack for '[' should be [:array]" do
+    assert StackBuilder.build_stack("[") == {:ok, [:array]}
+  end
+
+  @tag :skip
+  test "stack for '[{' should be [:object, :array]" do
+    assert StackBuilder.build_stack("[{") == {:ok, [:object, :array]}
+  end
+
+  @tag :skip
+  test "stack for '[{}' should be [:array]" do
+    assert StackBuilder.build_stack("[{}") == {:ok, [:array]}
+  end
 end
