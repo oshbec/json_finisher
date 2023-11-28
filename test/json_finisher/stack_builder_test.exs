ff3657f80644fbc9005e04b97c1ed61caaef3585
Josh Bechard <josh@oshbec.net>
2023-11-27 20:51:35 -0600

    Better test names

--- test/json_finisher/stack_builder_test.exs
+++ test/json_finisher/stack_builder_test.exs
@@ -2,139 +2,139 @@ defmodule JsonFinisher.StackBuilderTest do
   use ExUnit.Case, async: true
   import JsonFinisher.StackBuilder
 
-  test "stack for empty string should be empty" do
+  test "Empty string results in empty stack" do
     assert build_stack("") == {:ok, []}
   end
 
-  test "stack for '{' should be [:object]" do
+  test "Single opening brace '{' creates an object stack" do
     assert build_stack("{") == {:ok, [:object]}
   end
 
-  test "stack for '[' should be [:array]" do
+  test "Single opening bracket '[' creates an array stack" do
     assert build_stack("[") == {:ok, [:array]}
   end
 
-  test "stack for '[{' should be [:object, :array]" do
+  test "Combining '{' and '[' results in object within array stack" do
     assert build_stack("[{") == {:ok, [:object, :array]}
   end
 
-  test "stack for '[{}' should be [:array]" do
+  test "Opening and closing brace within brackets results in array stack" do
     assert build_stack("[{}") == {:ok, [:array]}
   end
 
-  test "stack for '[]' should be empty" do
+  test "Empty brackets '[]' result in empty stack" do
     assert build_stack("[]") == {:ok, []}
   end
 
-  test "returns an error for structural mismatch with input '{]'" do
+  test "Structural mismatch error with '{]' input" do
     assert build_stack("{]") == {:error, :structural_mismatch}
   end
 
-  test "returns an error for structural mismatch with input ']'" do
+  test "Structural mismatch error with lone ']' input" do
     assert build_stack("]") == {:error, :structural_mismatch}
   end
 
-  test "returns an error for structural mismatch with non-JSON starting input 'a'" do
+  test "Error on non-JSON starting character 'a'" do
     assert build_stack("a") == {:error, :structural_mismatch}
   end
 
-  test "handles whitespace around top-level object or array" do
+  test "Handling whitespace around empty JSON structures" do
     assert build_stack("  \r \n {} \n ") == {:ok, []}
     assert build_stack("\t\n   [] \t ") == {:ok, []}
   end
 
   describe "object keys and values" do
-    test "stack for '{\"key' should be [:key, :kv, :object]" do
+    test "Unfinished object key adds key, kv, and object to stack" do
       assert build_stack(~S|{"key|) == {:ok, [:key, :kv, :object]}
     end
 
-    test "stack for '{\"key\"' should be [:kv, :object]" do
+    test "Closed object key adds kv and object to stack" do
       assert build_stack(~S|{"key"|) == {:ok, [:kv, :object]}
     end
 
-    test "colon (:) signals that we are now in a value" do
+    test "Colon in object indicates start of value" do
       assert build_stack(~S|{"key":|) == {:ok, [:value, :kv, :object]}
     end
 
-    test "first `n` in a value indicates we're working on a null" do
+    test "Initial 'n' in object value indicates null start" do
       assert build_stack(~S|{"key":n|) == {:ok, [:null, :value, :kv, :object]}
       assert build_stack(~S|{"key":nu|) == {:ok, [:null, :value, :kv, :object]}
     end
 
-    test "first `l` within :null indicates we are almost done with :null value" do
+    test "Progressing through null spelling in object value" do
       assert build_stack(~S|{"key":nul|) ==
                {:ok, [:null_l1, :null, :value, :kv, :object]}
     end
 
-    test "second `l` in a :null value closes the :null, value, and :kv" do
+    test "Completing null value in object closes null, value, and kv" do
       assert build_stack(~S|{"key":null|) == {:ok, [:object]}
     end
 
-    test "first `t` in a value indicates we're working on a true" do
+    test "Initial 't' in value suggests true start in object" do
       assert build_stack(~S|{"key":t|) == {:ok, [true, :value, :kv, :object]}
       assert build_stack(~S|{"key":tr|) == {:ok, [true, :value, :kv, :object]}
       assert build_stack(~S|{"key":tru|) == {:ok, [true, :value, :kv, :object]}
     end
 
-    test "`e` in a :true closes :true, :value, and :kv" do
+    test "Completing true value in object closes true, value, and kv" do
       assert build_stack(~S|{"key":true|) == {:ok, [:object]}
     end
 
-    test "first `f` in a value indicates we're working on a :false" do
+    test "Initial 'f' in value indicates false start in object" do
       assert build_stack(~S|{"key":f|) == {:ok, [false, :value, :kv, :object]}
       assert build_stack(~S|{"key":fa|) == {:ok, [false, :value, :kv, :object]}
       assert build_stack(~S|{"key":fal|) == {:ok, [false, :value, :kv, :object]}
       assert build_stack(~S|{"key":fals|) == {:ok, [false, :value, :kv, :object]}
     end
 
-    test "`e` in a :false closes :false, :value, and :kv" do
+    test "Completing false value in object" do
       assert build_stack(~S|{"key":false|) == {:ok, [:object]}
     end
 
-    test "double-quote in a value begins a string" do
+    test "Double quote starts a string value in object" do
       assert build_stack(~S|{"key":"|) == {:ok, [:string, :value, :kv, :object]}
     end
 
-    test "double-quote in a :string closes :string, :value, :kv" do
+    test "Closing double quote finishes string value in object" do
       assert build_stack(~S|{"key":"hello"|) == {:ok, [:object]}
     end
 
-    test "a digit signals the start of a :number within a :value" do
+    test "Digit in object value starts number" do
       assert build_stack(~S|{"key":1|) == {:ok, [:number, :value, :kv, :object]}
     end
 
-    test "a } ends both the number and object" do
+    test "Closing brace ends number and object" do
       assert build_stack(~S|{"key":1.5}|) == {:ok, []}
     end
 
-    test "a non-digit signals the end of a :number within a :value" do
+    test "Non-digit character ends number in object value" do
       assert build_stack(~S|{"key":1.5 ,|) == {:ok, [:object]}
     end
 
-    test "a number based :string is not a :number" do
+    test "Numeric string in object is not a number" do
       assert build_stack(~S|{"key":"1.5|) == {:ok, [:string, :value, :kv, :object]}
     end
 
-    test "handles negative numbers correctly" do
+    test "Handling negative numbers in object" do
       assert build_stack(~S|{"key":-123|) == {:ok, [:number, :value, :kv, :object]}
     end
 
-    test "handles floating-point numbers correctly" do
+    test "Handling floating-point numbers in object" do
       assert build_stack(~S|{"key":123.456|) ==
                {:ok, [:number, :value, :kv, :object]}
     end
 
-    test "handles numbers with exponents correctly" do
+    test "Handling exponential numbers in object" do
       assert build_stack(~S|{"key":1e10|) == {:ok, [:number, :value, :kv, :object]}
       assert build_stack(~S|{"key":2.5E-4|) == {:ok, [:number, :value, :kv, :object]}
     end
 
-    test "handles numbers with plus sign in exponent correctly" do
+    test "Handling numbers with positive exponent in object" do
       assert build_stack(~S|{"key":1.23e+4|) ==
                {:ok, [:number, :value, :kv, :object]}
     end
 
-    test "handles zero and special number cases correctly" do
+    test "Handling zero and small numbers in object" do
       assert build_stack(~S|{"key":0|) == {:ok, [:number, :value, :kv, :object]}
       assert build_stack(~S|{"key":0.0001|) == {:ok, [:number, :value, :kv, :object]}
     end
