2f6c2751e1ea711ea3d3f3ac35ea30483f17faa8
Josh Bechard <josh@oshbec.net>
2023-12-04 10:11:15 -0600

    Close true, false, null

--- lib/json_finisher/closer.ex
+++ lib/json_finisher/closer.ex
@@ -3,25 +3,72 @@ defmodule JsonFinisher.Closer do
   Applies finishing touches to a partially complete JSON string based on a provided stack.
   """
 
-  def close_json(json_fragment, [:array | stack]) do
-    json_fragment = json_fragment <> "]"
-    close_json(json_fragment, stack)
+  def close_json(fragment, [:array | stack]) do
+    fragment = fragment <> "]"
+    close_json(fragment, stack)
   end
 
-  def close_json(json_fragment, [:object | stack]) do
-    json_fragment = json_fragment <> "}"
-    close_json(json_fragment, stack)
+  def close_json(fragment, [:object | stack]) do
+    fragment = fragment <> "}"
+    close_json(fragment, stack)
   end
 
-  def close_json(string_fragment, [:string | stack]) do
-    string_fragment = string_fragment <> ~S(")
-    close_json(string_fragment, stack)
+  def close_json(fragment, [:string | stack]) do
+    fragment = fragment <> ~S(")
+    close_json(fragment, stack)
   end
 
-  def close_json(string_fragment, [:key, :kv | stack]) do
-    string_fragment = string_fragment <> ~S(":null)
-    close_json(string_fragment, stack)
+  def close_json(fragment, [:key, :kv | stack]) do
+    fragment = fragment <> ~S(":null)
+    close_json(fragment, stack)
   end
 
-  def close_json(json_fragment, []), do: json_fragment
+  def close_json(fragment, [false | stack]) do
+    rest =
+      case String.last(fragment) do
+        "f" -> "alse"
+        "a" -> "lse"
+        "l" -> "se"
+        "s" -> "e"
+        _ -> ""
+      end
+
+    fragment = fragment <> rest
+    close_json(fragment, stack)
+  end
+
+  def close_json(fragment, [true | stack]) do
+    rest =
+      case String.last(fragment) do
+        "t" -> "rue"
+        "r" -> "ue"
+        "u" -> "e"
+        _ -> ""
+      end
+
+    fragment = fragment <> rest
+    close_json(fragment, stack)
+  end
+
+  def close_json(fragment, [:null_l1, :null | stack]) do
+    fragment = fragment <> ~S(l)
+    close_json(fragment, stack)
+  end
+
+  def close_json(fragment, [:null | stack]) do
+    rest =
+      case String.last(fragment) do
+        "n" -> "ull"
+        "u" -> "ll"
+        "l" -> "l"
+        _ -> ""
+      end
+
+    fragment = fragment <> rest
+    close_json(fragment, stack)
+  end
+
+  def close_json(fragment, [:value | stack]), do: close_json(fragment, stack)
+  def close_json(fragment, [:kv | stack]), do: close_json(fragment, stack)
+  def close_json(fragment, []), do: fragment
 end
--- test/json_finisher/closer_test.exs
+++ test/json_finisher/closer_test.exs
@@ -22,4 +22,28 @@ defmodule JsonFinisher.CloserTest do
   test "closes a key" do
     assert close_json(~S({"foo), [:key, :kv, :object]) == ~S({"foo":null})
   end
+
+  test "closes false values" do
+    assert close_json(~S({"foo":f), [false, :value, :kv, :object]) == ~S({"foo":false})
+    assert close_json(~S({"foo":fa), [false, :value, :kv, :object]) == ~S({"foo":false})
+    assert close_json(~S({"foo":fal), [false, :value, :kv, :object]) == ~S({"foo":false})
+    assert close_json(~S({"foo":fals), [false, :value, :kv, :object]) == ~S({"foo":false})
+    assert close_json(~S({"foo":false), [false, :value, :kv, :object]) == ~S({"foo":false})
+  end
+
+  test "closes true values" do
+    assert close_json(~S({"foo":t), [true, :value, :kv, :object]) == ~S({"foo":true})
+    assert close_json(~S({"foo":tr), [true, :value, :kv, :object]) == ~S({"foo":true})
+    assert close_json(~S({"foo":tru), [true, :value, :kv, :object]) == ~S({"foo":true})
+    assert close_json(~S({"foo":true), [true, :value, :kv, :object]) == ~S({"foo":true})
+  end
+
+  test "closes null values" do
+    assert close_json(~S({"foo":n), [:null, :value, :kv, :object]) == ~S({"foo":null})
+    assert close_json(~S({"foo":nu), [:null, :value, :kv, :object]) == ~S({"foo":null})
+    assert close_json(~S({"foo":nul), [:null, :value, :kv, :object]) == ~S({"foo":null})
+
+    assert close_json(~S|{"foo":nul|, [:null_l1, :null, :value, :value, :kv, :object]) ==
+             ~S({"foo":null})
+  end
 end
