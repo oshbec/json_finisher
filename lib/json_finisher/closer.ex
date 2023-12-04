380a3c8ebc8ed7dcb49f0241e920a8836717db77
Josh Bechard <josh@oshbec.net>
2023-12-04 09:39:33 -0600

    Init closer; basic array and object closing

--- lib/json_finisher/closer.ex
+++ lib/json_finisher/closer.ex
@@ -3,7 +3,15 @@ defmodule JsonFinisher.Closer do
   Applies finishing touches to a partially complete JSON string based on a provided stack.
   """
 
-  def close_json(_stack, json_fragment) do
-    json_fragment
+  def close_json(json_fragment, [:array | stack]) do
+    json_fragment = json_fragment <> "]"
+    close_json(json_fragment, stack)
   end
+
+  def close_json(json_fragment, [:object | stack]) do
+    json_fragment = json_fragment <> "}"
+    close_json(json_fragment, stack)
+  end
+
+  def close_json(json_fragment, []), do: json_fragment
 end
--- test/json_finisher/closer_test.exs
+++ test/json_finisher/closer_test.exs
@@ -1,5 +1,15 @@
 defmodule JsonFinisher.CloserTest do
   use ExUnit.Case, async: true
 
-  # Tests for JsonFinisher.Transformer module
+  import JsonFinisher.Closer
+
+  describe "brackets" do
+    test "closes open top-level array" do
+      assert close_json("[1, 2, 3", [:array]) == "[1, 2, 3]"
+    end
+
+    test "closes open top-level object" do
+      assert close_json("{", [:object]) == "{}"
+    end
+  end
 end
